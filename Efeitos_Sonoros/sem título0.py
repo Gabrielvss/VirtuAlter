502e2dcfc0c6f25c16fcef0d1a3216e2bd1c2487
Gabriel Vinicius <gvinicius178@gmail.com>
2019-08-13 14:32:21 -0300

    adicionando arquivos de som

---
+++ Efeitos_Sonoros/sem título0.py
@@ -0,0 +1,4 @@
+import os
+diretorio = os.getcwd()  
+os.startfile('C:\\Virtualter\\Kinect Files\\FAAST-1.2\\FAAST.exe')
+os.startfile('C:\\Virtualter\\Kinect Files\\virtualter\\team-bridge-client.exe')
